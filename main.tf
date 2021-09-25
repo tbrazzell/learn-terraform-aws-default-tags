2842510a136b0a993b4f4843abc916b98ce1943e
tbrazzell <90646155+tbrazzell@users.noreply.github.com>
2021-09-25 09:45:59 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,6 +33,10 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "example" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  tags = {
+    Service   = "Custom"
+    ManagedBy = "Resource"
+  }
 }
 
 resource "aws_launch_template" "example" {
